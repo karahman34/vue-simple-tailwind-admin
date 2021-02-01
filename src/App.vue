e2f9e2f1f7b43658ae8417b11d35e508e8205120
karahman <jakarahman007@gmail.com>
2021-02-01 19:38:23 +0700

    add layout system

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-    </div>
-    <router-view/>
+    <!-- Layout -->
+    <component :is="layout">
+      <!-- View -->
+      <router-view></router-view>
+    </component>
   </div>
 </template>
+
+<script>
+import DefaultLayout from '@/layouts/default/Layout'
+
+export default {
+  components: {
+    DefaultLayout,
+  },
+
+  computed: {
+    layout() {
+      const defaultLayout = 'default-layout'
+      const layoutRoute = this.$route.meta?.layout
+
+      return layoutRoute ? `${layoutRoute}-layout` : defaultLayout
+    },
+  },
+}
+</script>
 
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
 }
 </style>
---
+++ src/layouts/default/Layout.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <!-- Router View -->
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
